086a0838bf3ebf2ba1ee4602ff97354569bd8b0b
Philipp Lang <mantis.cert@siemens.com>
2015-02-11 13:24:06 +0100

    html for tags is now generated from the show_tagdisplay dingos templatetag.

--- mantis_actionables/templatetags/actionables_tags.py
+++ mantis_actionables/templatetags/actionables_tags.py
@@ -10,6 +10,7 @@ register = template.Library()
 def show_addTagInput_actionables(obj_id,curr_context):
     form = TagForm()
     form.fields['tag'].widget.attrs.update({
-        'data-obj-id': obj_id
+        'data-obj-id': obj_id,
+        'data-curr-context': curr_context,
         })
     return form.fields['tag'].widget.render('tag','')
