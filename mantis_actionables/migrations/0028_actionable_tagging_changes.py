2061e00b5358b03b5ce5cd505937edf01a757783
Philipp Lang <philipp@langonline.net>
2015-05-02 14:29:01 +0200

    added tag transfer to taggit to migration.

--- mantis_actionables/migrations/0028_actionable_tagging_changes.py
+++ mantis_actionables/migrations/0028_actionable_tagging_changes.py
@@ -3,16 +3,47 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import taggit.managers
+from mantis_actionables.models import TaggedActionableItem,ActionableTag,ActionableTaggingHistory
+
+tags_infos_to_transfer = None
+
+def extract_tag_infos_forward(apps, schema_editor):
+    global tags_infos_to_transfer
+    ActionableTag2X = apps.get_model("mantis_actionables","ActionableTag2X")
+    tags_infos_to_transfer = list(ActionableTag2X.objects.values('actionable_tag__tag__name','actionable_tag__context_id','content_type_id','object_id','actionable_tag_id'))
+
+
+def save_tags_forward(apps, schema_editor):
+    global tags_infos_to_transfer
+    tag_id_mapping = {}
+    for tag_info in tags_infos_to_transfer:
+        atag, created = ActionableTag.objects.get_or_create(context_id=tag_info['actionable_tag__context_id'],
+                                                            name=tag_info['actionable_tag__tag__name'])
+        tagged_actionable_item = TaggedActionableItem.objects.get_or_create(tag=atag,
+                                                                            content_type_id=tag_info['content_type_id'],
+                                                                            object_id=tag_info['object_id'])
+        tag_id_mapping[tag_info['actionable_tag_id']] = atag.id
+
+    #Update tag info in TaggingHistory
+    for entry in ActionableTaggingHistory.objects.all():
+        entry.tag_id = tag_id_mapping[entry.tag_id]
+        entry.save(update_fields=['tag_id'])
+
+    #delete old actionable tags
+    ActionableTag.objects.filter(name="to-delete").delete()
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('contenttypes', '0001_initial'),
         ('mantis_actionables', '0027_context_changes'),
     ]
 
     operations = [
+        migrations.RunPython(
+            extract_tag_infos_forward,
+            lambda x,y : None
+        ),
         migrations.CreateModel(
             name='TaggedActionableItem',
             fields=[
@@ -49,13 +80,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='actionabletag',
             name='name',
-            field=models.CharField(default='default-name', unique=True, max_length=100, verbose_name='Name'),
+            field=models.CharField(default='to-delete', unique=True, max_length=100, verbose_name='Name'),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='actionabletag',
             name='slug',
-            field=models.SlugField(default='default-slug', unique=True, max_length=100, verbose_name='Slug'),
+            field=models.SlugField(default='to-delete', unique=True, max_length=100, verbose_name='Slug'),
             preserve_default=False,
         ),
         migrations.AddField(
@@ -80,5 +111,9 @@ class Migration(migrations.Migration):
         ),
         migrations.DeleteModel(
             name='TagName',
+        ),
+        migrations.RunPython(
+            save_tags_forward,
+            lambda x,y : None
         ),
     ]
